0cd8a35c389f95179decd6d9512cffe5e42c2eeb
Antonio <3rd.antonio.santos@b2wdigital.com>
2022-12-06 19:30:00 -0300

    Add Tests Features and Refactor Project Structure

--- utils/basic.py
+++ utils/basic.py
@@ -7,7 +7,7 @@ Author: Antonio Pedro Santos Alves
 Reviewer: Marcos Kalinowski
 """
 
-def rename_values(rename_mapping: dict, values: list[any]) -> list[any]:
+def rename_values(*, rename_mapping: dict, values: list[any], not_found_value: str = '') -> list[any]:
     """
         Rename values from a list according to a dict 
         with new values labels
@@ -15,12 +15,12 @@ def rename_values(rename_mapping: dict, values: list[any]) -> list[any]:
     renamed_values = []
     
     for value in values:
-        renamed_values.append(rename_mapping.get(value, 'Not selected'))
+        renamed_values.append(rename_mapping.get(value, not_found_value if not_found_value else value))
     
     return renamed_values
 
 
-def format_wordcloud_text(texts: list[str], use_sep: bool) -> dict:
+def format_wordcloud_text(*, texts: list[str], use_sep: bool) -> dict:
     """
         Build a dict with a word and its importance/weight 
         in the corpus.
